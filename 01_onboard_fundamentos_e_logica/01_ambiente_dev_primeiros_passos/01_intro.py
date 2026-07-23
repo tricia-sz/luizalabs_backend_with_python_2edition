da55a8debf695d84fb52dee7692c0ab3e09f25a0
Patricia Souza <patricia.sz.br@gmail.com>
2026-07-23 17:33:45 -0300

    intro py

---
+++ 01_onboard_fundamentos_e_logica/01_ambiente_dev_primeiros_passos/01_intro.py
@@ -0,0 +1,10 @@
+'''
+  - Oriegem: 1989
+  - Criador Guido Van Rossum
+  - Influenciada por ABC, linguagem para iniciantes.
+  - Facil, intuitiva
+  - codigo aberto
+  - Codigo legivel quanto ingles
+  - Adequada para tarefas diarias e produtiva.
+  
+'''
