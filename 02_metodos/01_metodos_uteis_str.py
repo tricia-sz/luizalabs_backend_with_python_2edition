879557786fd8182802b5076bb187661c07754813
Patricia Souza <patricia.sz.br@gmail.com>
2026-07-24 14:44:47 -0300

    Metodos de string

---
+++ 02_metodos/01_metodos_uteis_str.py
@@ -0,0 +1,23 @@
+'''
+  - Interpolacao de variaveis
+  - Fatiamento de strings
+  - String multiplas linhas
+'''
+# Maiusculas, minusculas e titulo
+curso = 'pYtHon'
+
+print(curso.upper()) # PYTHON
+print(curso.lower()) # python
+print(curso.title()) # Python
+
+print(curso.strip()) # Tira espaços em branco da esquerda e direita
+print(curso.lstrip()) # Tira espaços em branco da esquerda
+print(curso.rstrip()) # Tira espaços em branco  direita
+
+print(curso.center(10, "*")) # *****Python*****
+print("_".join(curso)) # P_y_t_h_o_n
+
+
+
+
+
