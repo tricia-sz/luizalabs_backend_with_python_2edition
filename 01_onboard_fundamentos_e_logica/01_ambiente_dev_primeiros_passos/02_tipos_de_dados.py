4c1e13f1dd5f76eba2d57d82b1c6382402ecc019
Patricia Souza <patricia.sz.br@gmail.com>
2026-07-24 14:13:47 -0300

    Tipos de dados

---
+++ 01_onboard_fundamentos_e_logica/01_ambiente_dev_primeiros_passos/02_tipos_de_dados.py
@@ -0,0 +1,16 @@
+'''
+  Tipos de dados
+
+  Texto - str
+  Numero - int, float, complex
+  Sequencia - list, tuple, range
+  Mapa - dict
+  Colecao - set, fronzenset
+  Booelano - bool
+  Binário - byte, bytebynary, memoryview
+'''
+
+print("1+10")
+print(1.3 +10)
+print(True)
+print(False)
